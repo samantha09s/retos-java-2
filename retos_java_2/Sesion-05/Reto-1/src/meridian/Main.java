0416fade87713ff9fb6bb49165780e88751aa00e
Samantha Munguía <140031528+samantha09s@users.noreply.github.com>
2025-06-15 17:24:58 -0600

    Delete retos_java_2/Sesion-05/Reto-1 directory

--- retos_java_2/Sesion-05/Reto-1/src/meridian/Main.java
+++
@@ -1,8 +0,0 @@
-package meridian;
-
-// Punto de entrada para simular el monitoreo en tiempo real de sistemas urbanos críticos.
-public class Main {
-    public static void main(String[] args) {
-        new SistemaMeridian().iniciar();
-    }
-}
